7a471ad9c74d179df0c0cc3f5f88c127f9a208b6
Meena-jman <meena.k@jmangroup.com>
2025-02-11 14:19:11 +0530

    Update

--- backend/server.js
+++ backend/server.js
@@ -4,11 +4,11 @@ const mongoose=require('mongoose');
 const dotenv=require('dotenv');
 dotenv.config();
 const app=express();
-const port = process.env.PORT || 5000;
+
 
 
 app.use(express.json());
-// app.use(cors());
+
 app.use(cors({ origin: 'http://localhost:5173' }));
 
 mongoose.connect(process.env.MONGO_URI, {
@@ -81,5 +81,5 @@ app.delete('/api/blogs/:id', async (req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
